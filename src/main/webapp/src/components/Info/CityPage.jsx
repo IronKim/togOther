984139121db21dca7a02a371d0df3590e52a73e9
fantazina <137284904+fantazina@users.noreply.github.com>
2023-11-20 15:29:46 +0900

    도시페이지 환율,날씨 최종

--- src/main/webapp/src/components/Info/CityPage.jsx
+++ src/main/webapp/src/components/Info/CityPage.jsx
@@ -3,22 +3,26 @@ import React, { useEffect, useState } from 'react';
 import styles from '../../css/Info/CityPage.module.css';
 import Button from 'react-bootstrap/Button';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
-import CityMoneyApi from './CityMoneyApi';
+import CityMoneyApi from './CityMoneyApi';  
 import { useNavigate, useParams } from 'react-router-dom';
 import { getCityBySeq } from '../../api/CityApiService';
 import { getPlaceListByCitySeq } from '../../api/PlaceApiService';
 import WeatherIcon from '../../constants/WeatherIcon';
 import weatherData from '../../constants/WeatherData';
-const CityPage = () => {
+
+const CityPage = () => { 
     const { citySeq } = useParams();
+
     const [city, setCity] = useState('');
     const [placeData, setPlaceData] = useState(['']);
     const [filteredPlaceData, setFilteredPlaceData] = useState([]);
     const [activeButton, setActiveButton] = useState('TouristSpot');
     const [search, setSearch] = useState('');
-    const [selectedCity, setSelectedCity] = useState('');
+    const [selectedCity, setSelectedCity] = useState(''); 
     const [cityWeather, setCityWeather] = useState(null); // 날씨 정보를 저장할 상태
+
     useEffect(() => {
+
         getCityBySeq(citySeq)
             .then(res => {
                 setCity(res.data);
@@ -26,6 +30,7 @@ const CityPage = () => {
                 setSelectedCity(res.data.cityName); // 선택된 도시 정보 업데이트
             })
             .catch(e => console.log(e));
+
         getPlaceListByCitySeq(citySeq)
             .then(res => {
                 setPlaceData(res.data);
@@ -33,20 +38,25 @@ const CityPage = () => {
             })
             .catch(e => console.log(e));
     }, [citySeq, activeButton]);
+
     const handleButtonClick = (buttonName) => {
         setActiveButton(buttonName);
-        const filteredData = buttonName === 'TouristSpot' ? placeData.filter(item => item.code === 1) :
+        const filteredData = buttonName === 'TouristSpot' ? placeData.filter(item => item.code === 1) : 
                              buttonName === 'store' ? placeData.filter(item => item.code === 0) :
                              placeData;
         setFilteredPlaceData(filteredData);
     };
+
     const navigate = useNavigate();
+
     const onToPlacePage = (placeSeq) => {
         navigate(`/info/place/${placeSeq}`);
     }
+
     const onChange = (e) => {
         setSearch(e.target.value)
     }
+
     const selectCity = (citySeq) => {
         getCityBySeq(citySeq)
             .then(res => {
@@ -54,73 +64,69 @@ const CityPage = () => {
             })
             .catch(e => console.log(e));
     }
+
     //날씨 정보를 받아오는 함수
     const handleWeatherData = (weatherData) => {
         setCityWeather(weatherData);
     };
+
     return (
-        <div>
+        <>
             <img src={city.cityImage} className={ styles.citypage }/>
+            <div className={styles.main}>
             <div className={ styles.cityName }>{city.cityName}</div>
             {/* ----------------검색---------------- */}
             {/* <div className={ styles.inputBox }>
                 <input className={ styles.input } type= 'text' placeholder='Search...'/>
                 <img src="https://s3.ap-northeast-2.amazonaws.com/cdn.wecode.co.kr/icon/search.png" className={ styles.img }></img>
             </div> */}
+
+
             <div className={styles.api}>
-                {/* 도시의 환율 정보를 표시 */}
-                <div className={styles.money}><CityMoneyApi selectedCity={ selectedCity } /></div>
             {/*  날씨 정보 표시 */}
             <div className={styles.weather}>
-                <CityWeatherApi selectedCity={selectedCity} onWeatherData={handleWeatherData} />
+                <div className={styles.weatherCity}>
+                    <CityWeatherApi selectedCity={selectedCity} onWeatherData={handleWeatherData} />
                 </div>
+
                 {/* 받아온 날씨 정보 icon으로 출력 */}
-                <div className={styles.weatherIcon}>
+                <div className={styles.weatherIcon} >
                     {cityWeather && cityWeather.weather && cityWeather.weather.length > 0 && (
                         <div>
-                            <WeatherIcon weatherIconCode={cityWeather.weather[0].icon} />
+                            <WeatherIcon weatherIconCode={cityWeather.weather[0].icon}></WeatherIcon>
                             <p>{` ${weatherData[cityWeather.weather[0].id].description}`}</p>
                         </div>
-                    )}
+                        )}
                 </div>
             </div>
-            <div className={styles.button} style={{ position: 'relative' }}>
-                <ButtonGroup aria-label='Basic example' style={{ position: 'relative', top: '3em' }}>
-                    <Button
-                        variant={activeButton === 'TouristSpot' ? 'primary' : 'light'}
-                        onClick={() => handleButtonClick('TouristSpot')}
-                    >
-                        명소
-                    </Button>
-                    <Button
-                        variant={activeButton === 'store' ? 'primary' : 'light'}
-                        onClick={() => handleButtonClick('store')}
-                    >
-                        맛집
-                    </Button>
-                </ButtonGroup>
-                <hr style={{width:'70%', position: 'absolute', top: '130%', zIndex: -1 }}/>
+                    {/* 도시의 환율 정보를 표시 */}
+                    <div className={styles.money}><CityMoneyApi selectedCity={ selectedCity } /></div>
             </div>
-            <div style={{width:1200, margin: '0 auto'}}>
+            <div className={styles.toggle} style={{paddingLeft: activeButton === 'TouristSpot' ? '3px' : '120px' }}
+            onClick={() => activeButton === 'TouristSpot' ? handleButtonClick('store') : handleButtonClick('TouristSpot') }>
+                    <div className={styles.toggleLeft} style={{marginLeft: activeButton === 'TouristSpot' ? '43px' : '-74px',
+                    color: activeButton === 'TouristSpot' ?'white' : 'black'}}>명소</div>
+                    <div className={styles.toggleRight} style={{marginLeft: activeButton === 'TouristSpot' ? '161px' : '44px',
+                    color: activeButton === 'store' ?'white' : 'black' }}>맛집</div>
+                    <div className={styles.toggleBox}></div>
+            </div>
                 {
                 filteredPlaceData.map((item, index) => (
-                        <div key={index} style={{ display: 'inline-block', marginBottom: '2em', justifyContent: 'center', userSelect: 'none'}}>
-                            <div>
-                                <div className={styles.list1} onClick={() => onToPlacePage(item.placeSeq)}>
+                                <div key={index} className={styles.list1} onClick={() => onToPlacePage(item.placeSeq)}>
                                     <div className={styles.imgDiv}>
-                                        <img src={item.image} style={{ width: '100%', height: '62.4%', borderRadius:16, userSelecter: 'none'}} alt={item.name} />
+                                        <img src={item.image} style={{borderRadius:16, userSelecter: 'none'}} alt={item.name} />
+
                                     </div>
                                     <div className={styles.textDiv}>
-                                        <div style={{ fontSize: 20, position: 'relative', top: '5%' }}>{item.name}</div>
+                                        <div className={styles.textName}>{item.name}</div>
                                         <div className={styles.textDiv1}>{item.context1}</div>
                                     </div>
                                 </div>
-                            </div>
-                        </div>
                     ))
                 }
             </div>
-        </div>
+        </>
     );
 };
+
 export default CityPage;
