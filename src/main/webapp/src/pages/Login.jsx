8ebaefea8b378b65bead5192dd12129acc6da41c
IronKim <tankokim@gmail.com>
2023-11-22 17:40:18 +0900

    도시페이지 css수정 병합

--- src/main/webapp/src/pages/Login.jsx
+++ src/main/webapp/src/pages/Login.jsx
@@ -3,23 +3,31 @@ import styles from '../css/login.module.css';
 import { useNavigate } from 'react-router-dom';
 import { loginUser } from '../api/UserApiService';
 import { useUserStore } from '../stores/mainStore';
-import { MdOutlineAlternateEmail } from "react-icons/md";
 import loginFormImage from '../assets/image/loginFormImage.png';
 
 const Login = () => {
 
   const navigate = useNavigate();
 
-  const {user, setUser} = useUserStore();
+  const {setUser} = useUserStore();
 
   const [loginDTO, setLoginDTO] = useState({
     email: '',
     pwd: ''
   });
 
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+
   const oninput = (e) => {
     const { name, value } = e.target;
-    
+
+    if (name === 'email') {
+      setEmailError('');
+    } else if (name === 'pwd') {
+      setPasswordError('');
+    }
+
     setLoginDTO({
       ...loginDTO,
       [name]: value
@@ -27,8 +35,42 @@ const Login = () => {
 
   }
 
+
+  const validateEmail = (email) => {
+    const emailPattern = /^[a-zA-Z0-9_]+@[a-z0-9.-]+\.[a-z]{2,}$/;
+    return emailPattern.test(String(email));
+  };
+
+  const validateInput = () => {
+    let isValid = true;
+  
+    if (!loginDTO.email) {
+      setEmailError('이메일을 입력해주세요.');
+      isValid = false;
+    }else if (!validateEmail(loginDTO.email)) {
+      setEmailError('이메일 형식이 올바르지 않습니다.');
+      isValid = false;
+    } else {
+      setEmailError('');
+    }
+  
+    if (!loginDTO.pwd) {
+      setPasswordError('비밀번호를 입력해주세요.');
+      isValid = false;
+    } else {
+      setPasswordError('');
+    }
+  
+    return isValid;
+  }
+
+  
   const onsubmit = (e) => {
     e.preventDefault();
+
+    if (!validateInput()) {
+      return;
+    }
 
     loginUser(loginDTO)
     .then(res => {
@@ -73,20 +115,29 @@ const Login = () => {
         <img className={styles.loginFormImage} src={loginFormImage} alt="loginFormImage" />
         
         <div className={styles.loginBox}>
-          <p style={{fontSize: '3.4em', marginTop: '1em', marginBottom: '3em'}}>로그인</p>
+          <p style={{fontSize: '3.4em', marginTop: '0.7em', marginBottom: '0.4em'}}>로그인</p>
           <form>
-            <div className={styles.inputBar}>
-              <label htmlFor='email'>Email</label>
+            <div className={styles.formGroup}>
               <input type="text" name="email" value={loginDTO.email} onChange={(e)=> oninput(e)} required />
-              <MdOutlineAlternateEmail className={styles.boxIcon} />
+              <label htmlFor='email'>이메일</label>
             </div>
-            <div className={styles["user-box"]}>
+            <div className={styles.inputError}>{emailError || 'ㅤ'}</div>
+            <div className={styles.formGroup}>
               <input type="password" name="pwd" value={loginDTO.pwd} onChange={(e)=> oninput(e)} required />
-              <label>Password</label>
+              <label htmlFor='pwd'>비밀번호</label>
             </div>
-            <button type="submit" onClick={onsubmit}>로그인</button>
+            <div className={styles.inputError}>{passwordError || 'ㅤ'}</div>
+            <div style={{display: 'flex', justifyContent: 'space-between', marginTop: '1em'}}>
+              <span style={{fontSize: '1.3em'}}>아이디 / 비밀번호 찾기</span>
+              <span style={{fontSize: '1.3em'}}>회원가입</span>
+            </div>
           </form>
-            <button type="button" onClick={() => navigate('/advisor')}>관리자페이지로</button>
+            <button className={styles.loginBtn} type="submit" onClick={onsubmit}>로그인</button>
+          <div style={{display: 'flex', justifyContent: 'space-around', width: '80%', marginTop: '3em'}}>
+              <img src="https://img.icons8.com/ios/50/000000/google-logo.png" alt="google" />
+              <img src="https://img.icons8.com/ios/50/000000/facebook-new.png" alt="facebook" />
+              <img src="https://img.icons8.com/ios/50/000000/kakao-talk.png" alt="kakao" />
+          </div>
         </div>
       </div>
     </div>
