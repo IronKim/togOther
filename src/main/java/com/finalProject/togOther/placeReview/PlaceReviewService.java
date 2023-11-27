6aab9375cef2cb57890197376d41a29b0972e79d
IronKim <tankokim@gmail.com>
2023-11-27 13:53:06 +0900

    리뷰작성 하고 유저 정보불러오기

--- src/main/java/com/finalProject/togOther/placeReview/PlaceReviewService.java
+++ src/main/java/com/finalProject/togOther/placeReview/PlaceReviewService.java
@@ -1,15 +1,13 @@
 package com.finalProject.togOther.placeReview;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 
 import com.finalProject.togOther.dto.PlaceReviewDTO;
 
 public interface PlaceReviewService {
 
-	public ResponseEntity<List<PlaceReviewDTO>> getPlaceReviewByPlaceSeq(int placeSeq);
+	public ResponseEntity<?> getPlaceReviewByPlaceSeq(int placeSeq);
 
-	public ResponseEntity<String> addPlaceReview(PlaceReviewDTO placeReviewDTO);
+	public ResponseEntity<?> addPlaceReview(PlaceReviewDTO placeReviewDTO);
 
 }
