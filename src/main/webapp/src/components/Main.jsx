bbe2662c86fc1620538d1bf90d20598534d5b797
simin <inho1019@naver.com>
2023-11-30 16:19:17 +0900

    결제 구현중

--- src/main/webapp/src/components/Main.jsx
+++ src/main/webapp/src/components/Main.jsx
@@ -87,7 +87,7 @@ const Main = ({ showNavbar }) => {
                         <Route path='' element={ <PackageMain />} />
                         <Route path='List/:searchData' element={ <PackageLists />} />
                         <Route path='details/:tpSeq' element= { <PackageDetails/> } />
-                        <Route path='reservation' element={ <PackgeReservation /> } />
+                        <Route path='reservation/:packageSeq/:info' element={  <UserRoute><PackgeReservation/></UserRoute>} />
                     </Route>
                     {showNavbar && <BottomNav showNavbar={showNavbar} />}
                 </Routes>
